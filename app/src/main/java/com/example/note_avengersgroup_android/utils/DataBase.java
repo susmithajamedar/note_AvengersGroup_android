e257a385e77d2aa6ca58c952fed7c74a3c14657d
Susmitha Jamedar <84050055+susmithajamedar@users.noreply.github.com>
2021-06-13 12:43:00 -0400

    Added DB function to Insert Note
    Added a Database method to Insert Note into Notes Table

--- app/src/main/java/com/example/note_avengersgroup_android/utils/DataBase.java
+++ app/src/main/java/com/example/note_avengersgroup_android/utils/DataBase.java
@@ -6,6 +6,8 @@ import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
+import com.example.note_avengersgroup_android.models.NoteModel;
+
 import java.util.ArrayList;
 
 public class DataBase extends SQLiteOpenHelper {
@@ -52,4 +54,23 @@ public class DataBase extends SQLiteOpenHelper {
         }
         return array_list;
     }
+
+    public boolean insertNote(NoteModel noteModel) {
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues contentValues = new ContentValues();
+        contentValues.put("category", noteModel.getCategory());
+        contentValues.put("note", noteModel.getNote());
+        contentValues.put("annotation", noteModel.getAnnotation());
+        contentValues.put("imageOne", noteModel.getImageOne());
+        contentValues.put("imageTwo", noteModel.getImageTwo());
+        contentValues.put("audio", noteModel.getAudio());
+        contentValues.put("location", noteModel.getLocation());
+        contentValues.put("date", noteModel.getDate());
+        db.insert(Constants.TABLE_NOTES, null, contentValues);
+        return true;
+    }
+
+    
+
+
 }
