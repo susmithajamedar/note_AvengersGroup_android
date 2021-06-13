f037e9f726ceba5cbd05d2aad26b1ed522c2b25f
Susmitha Jamedar <84050055+susmithajamedar@users.noreply.github.com>
2021-06-13 03:23:23 -0400

    Added Database constants
    Added DataBase Constants in a Common utils package

--- app/src/main/java/com/example/note_avengersgroup_android/MainActivity.java
+++ app/src/main/java/com/example/note_avengersgroup_android/MainActivity.java
@@ -8,6 +8,6 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.data_saved_success);
+        setContentView(R.layout.activity_main);
     }
 }
