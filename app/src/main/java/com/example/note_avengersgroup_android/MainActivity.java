6f56a5322fbb57f08e80cc3e108a1dd99271b8ed
Mohit Nadipineni <84355183+MohitNadipineni@users.noreply.github.com>
2021-06-13 10:57:35 -0400

    Design for Add Notes Navigatin
    Design for Add Notes Navigation

--- app/src/main/java/com/example/note_avengersgroup_android/MainActivity.java
+++ app/src/main/java/com/example/note_avengersgroup_android/MainActivity.java
@@ -36,7 +36,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.select_sort_by);
+        setContentView(R.layout.activity_add_notes);
         //initialize all action item views & set  Permissions
         //init();
     }
