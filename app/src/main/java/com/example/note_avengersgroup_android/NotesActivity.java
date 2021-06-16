67df9ea954d47124aa2469fee6044a6104d85d73
Jaspinder Singh <jaspindersingh24@gmail.com>
2021-06-16 00:47:43 -0400

    Added Back Navigation to Category Page
    Added Back Navigation to Category Page

--- app/src/main/java/com/example/note_avengersgroup_android/NotesActivity.java
+++ app/src/main/java/com/example/note_avengersgroup_android/NotesActivity.java
@@ -2,21 +2,32 @@ package com.example.note_avengersgroup_android;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class NotesActivity extends AppCompatActivity implements View.OnClickListener {
 
-
+    TextView backTV;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_notes);
+        init();
+    }
+    public void init() {
+        backTV = findViewById(R.id.backTV);
+        backTV.setOnClickListener(this);
     }
     @Override
     public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.backTV:
+                finish();
+                break;
 
+        }
     }
 }
