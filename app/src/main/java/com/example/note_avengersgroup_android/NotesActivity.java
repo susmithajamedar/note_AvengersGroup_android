9a05bbe4986453c9dadda7b46338472d895ad489
Jaspinder Singh <jaspindersingh24@gmail.com>
2021-06-16 01:21:59 -0400

    Added link to sort by dialog
    Link Sort by component and dialog

--- app/src/main/java/com/example/note_avengersgroup_android/NotesActivity.java
+++ app/src/main/java/com/example/note_avengersgroup_android/NotesActivity.java
@@ -8,10 +8,12 @@ import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.note_avengersgroup_android.utils.Constants;
+import com.google.android.material.bottomsheet.BottomSheetDialog;
 
 public class NotesActivity extends AppCompatActivity implements View.OnClickListener {
     String category;
     TextView backTV;
+    TextView sortBy;
     TextView addTV;
 
 
@@ -19,6 +21,7 @@ public class NotesActivity extends AppCompatActivity implements View.OnClickList
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_notes);
+        category = getIntent().getStringExtra("selected_category");
         init();
     }
     public void init() {
@@ -26,6 +29,8 @@ public class NotesActivity extends AppCompatActivity implements View.OnClickList
         addTV = findViewById(R.id.addTV);
         backTV.setOnClickListener(this);
         addTV.setOnClickListener(this);
+        sortBy = findViewById(R.id.sort_by_date);
+        sortBy.setOnClickListener(this);
     }
     @Override
     public void onClick(View v) {
@@ -40,6 +45,35 @@ public class NotesActivity extends AppCompatActivity implements View.OnClickList
                 startActivity(intent);
                 break;
 
+            case R.id.sort_by_date:
+                openBottomDialog();
+                break;
+
         }
+    }
+
+    private void openBottomDialog() {
+        final BottomSheetDialog dialog = new BottomSheetDialog(NotesActivity.this, R.style.bottom_dialog_theme);
+        dialog.setContentView(R.layout.select_sort_by);
+        dialog.setCanceledOnTouchOutside(true);
+        dialog.show();
+        TextView tvSortByDate = dialog.findViewById(R.id.tv_sort_by_date);
+        TextView tvSortByName = dialog.findViewById(R.id.tv_sort_by_name);
+
+        tvSortByDate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                sortBy.setText("Sort by date");
+                dialog.dismiss();
+            }
+        });
+        tvSortByName.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                sortBy.setText("Sort by name");
+                dialog.dismiss();
+            }
+        });
+
     }
 }
