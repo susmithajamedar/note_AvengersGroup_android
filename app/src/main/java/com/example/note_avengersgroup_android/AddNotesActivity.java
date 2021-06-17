44d57933aa9051f47a505cd1756e2a0268c3b4a1
Vijay Lingamaneni <84323645+VijayLingamaneni@users.noreply.github.com>
2021-06-17 15:43:10 -0400

    Added code Location GPS Tracker in AddNotes Page
    Added code Location GPS Tracker in AddNotes Page

--- app/src/main/java/com/example/note_avengersgroup_android/AddNotesActivity.java
+++ app/src/main/java/com/example/note_avengersgroup_android/AddNotesActivity.java
@@ -5,14 +5,19 @@ import android.view.View;
 import android.widget.ImageView;
 import android.widget.Spinner;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.note_avengersgroup_android.models.NoteModel;
 import com.example.note_avengersgroup_android.utils.Constants;
 import com.example.note_avengersgroup_android.utils.DataBase;
+import com.example.note_avengersgroup_android.utils.GPSTracker;
 
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
 
 public class AddNotesActivity extends AppCompatActivity implements View.OnClickListener {
     Spinner spCategory;
@@ -21,8 +26,11 @@ public class AddNotesActivity extends AppCompatActivity implements View.OnClickL
     String category;
     NoteModel model;
     ImageView  ivBack;
+    GPSTracker gpsTracker;
+    double userLat, userLng;
     private String flag = null;
     private NoteModel noteModel;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -45,7 +53,23 @@ public class AddNotesActivity extends AppCompatActivity implements View.OnClickL
         ivBack = findViewById(R.id.iv_back);
         ivBack.setOnClickListener(this);
         tvSpinner = findViewById(R.id.tv_spinner);
+        gpsTracker = new GPSTracker(this);
+        userLat = gpsTracker.getLatitude();
+        userLng = gpsTracker.getLongitude();
+        model.setLocation("" + userLat + " " + userLng);
+        model.setDate(getCurrentTime());
+        Toast.makeText(this, "lat " + userLat + "  lng" + userLng, Toast.LENGTH_LONG).show();
+        Toast.makeText(this, "date " + getCurrentTime(), Toast.LENGTH_LONG).show();
 
+    }
+    private String getCurrentTime() {
+        String formatedDate = "";
+        Date currentTime = Calendar.getInstance().getTime();
+        String newFormat = "dd-MM-yyyy hh:mm aaa";
+        SimpleDateFormat timeFormat = new SimpleDateFormat(newFormat);
+        formatedDate = timeFormat.format(currentTime);
+        //tvCurrentDateTime.setText(getString(R.string.text_current_time_is) + "  " + formatedDate)
+        return formatedDate;
     }
         @Override
     public void onClick(View v) {
